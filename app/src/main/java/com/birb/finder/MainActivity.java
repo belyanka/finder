edc38d55c0ec739b0659d6c08e1c02f70dd80b62
Ольга <belyankamew@gmail.com>
2017-03-21 23:15:47 +0300

    activities initiated

--- app/src/main/java/com/birb/finder/MainActivity.java
+++ app/src/main/java/com/birb/finder/MainActivity.java
@@ -2,12 +2,22 @@ package com.birb.finder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button searchButton;
+    private ImageView imageIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchButton = (Button) findViewById(R.id.search);
+        imageIcon = (ImageView) findViewById(R.id.icon);
+
+        imageIcon.setImageResource(R.drawable.iconbird);
     }
 }
---
+++ app/src/main/java/com/birb/finder/SearchActivity.java
@@ -0,0 +1,7 @@
+package com.birb.finder;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+
+}
